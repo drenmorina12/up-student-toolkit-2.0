f7b891ad63ed361f6aaf7b492b5b9db2df21caf1
Endrit Kastrati <endritkastrati8@gmail.com>
2024-04-14 20:08:26 +0200

    Adding auto-slider and slide style

--- rreth-nesh/rreth-nesh.js
+++ rreth-nesh/rreth-nesh.js
@@ -1,21 +1,81 @@
-const themelimiText = document.querySelector(".themelimi");
+const wrapper = document.querySelector(".wrapper");
+const carousel = document.querySelector(".carousel");
+const firstCardWidth = carousel.querySelector(".card").offsetWidth;
+const arrowBtns = document.querySelectorAll(".wrapper i");
+const carouselChildrens = [...carousel.children];
 
-const dataEThemelimit = new Date("December 23, 1970");
-const currentDate = new Date();
+let isDragging = false, isAutoPlay = true, startX, startScrollLeft, timeoutId;
 
-const yearsSinceOpening =
-  currentDate.getFullYear() - dataEThemelimit.getFullYear();
+// Get the number of cards that can fit in the carousel at once
+let cardPerView = Math.round(carousel.offsetWidth / firstCardWidth);
 
-themelimiText.textContent = `Universiteti i Prishtinës ofron shërbime cilësore që prej ${yearsSinceOpening} vitesh.`;
+// Insert copies of the last few cards to beginning of carousel for infinite scrolling
+carouselChildrens.slice(-cardPerView).reverse().forEach(card => {
+    carousel.insertAdjacentHTML("afterbegin", card.outerHTML);
+});
 
-// replace() :
-let str = document.getElementById("demoo").innerHTML; 
-let res = str.replace(/HAPJE/g, "MBYLLJE");
-document.getElementById("demoo").innerHTML = res;
-// Zevendsojme fjalen HAPJE me fjalen MBYLLJE ne display ne fund te rreth nesh del-  NË MBYLLJE :
+// Insert copies of the first few cards to end of carousel for infinite scrolling
+carouselChildrens.slice(0, cardPerView).forEach(card => {
+    carousel.insertAdjacentHTML("beforeend", card.outerHTML);
+});
 
-// match() :
-let text = "UP Student Toolkit";
-let result = text.match("z"); 
-document.getElementById("demoo2").innerHTML = result;     
-// letra z nuk gjindet ne text=UP Student Toolkit prandaj nuk shfaqet asgje ne ekran.
+// Scroll the carousel at appropriate postition to hide first few duplicate cards on Firefox
+carousel.classList.add("no-transition");
+carousel.scrollLeft = carousel.offsetWidth;
+carousel.classList.remove("no-transition");
+
+// Add event listeners for the arrow buttons to scroll the carousel left and right
+arrowBtns.forEach(btn => {
+    btn.addEventListener("click", () => {
+        carousel.scrollLeft += btn.id == "left" ? -firstCardWidth : firstCardWidth;
+    });
+});
+
+const dragStart = (e) => {
+    isDragging = true;
+    carousel.classList.add("dragging");
+    // Records the initial cursor and scroll position of the carousel
+    startX = e.pageX;
+    startScrollLeft = carousel.scrollLeft;
+}
+
+const dragging = (e) => {
+    if(!isDragging) return; // if isDragging is false return from here
+    // Updates the scroll position of the carousel based on the cursor movement
+    carousel.scrollLeft = startScrollLeft - (e.pageX - startX);
+}
+
+const dragStop = () => {
+    isDragging = false;
+    carousel.classList.remove("dragging");
+}
+
+const infiniteScroll = () => {
+    if(carousel.scrollLeft === 0) {
+        carousel.classList.add("no-transition");
+        carousel.scrollLeft = carousel.scrollWidth - (2 * carousel.offsetWidth);
+        carousel.classList.remove("no-transition");
+    }
+    else if(Math.ceil(carousel.scrollLeft) === carousel.scrollWidth - carousel.offsetWidth) {
+        carousel.classList.add("no-transition");
+        carousel.scrollLeft = carousel.offsetWidth;
+        carousel.classList.remove("no-transition");
+    }
+
+    clearTimeout(timeoutId);
+    if(!wrapper.matches(":hover")) autoPlay();
+}
+
+const autoPlay = () => {
+    if(window.innerWidth < 800 || !isAutoPlay) return; 
+    // Cdo 2500 ms autoplay
+    timeoutId = setTimeout(() => carousel.scrollLeft += firstCardWidth, 2000);
+}
+autoPlay();
+
+carousel.addEventListener("mousedown", dragStart);
+carousel.addEventListener("mousemove", dragging);
+document.addEventListener("mouseup", dragStop);
+carousel.addEventListener("scroll", infiniteScroll);
+wrapper.addEventListener("mouseenter", () => clearTimeout(timeoutId));
+wrapper.addEventListener("mouseleave", autoPlay);
