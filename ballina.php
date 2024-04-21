cd70345419212bce90a7a553efec69708c061d11
Dren Morina <drenmorina321@gmail.com>
2024-04-21 03:31:26 +0200

    Remove self-adding letter script

--- ballina.php
+++ ballina.php
@@ -103,30 +103,6 @@
       AOS.init();
     </script>
 
-    <script>
-      var selfAddingTextElement = document.getElementById("selfAddingText");
-
-      // Initial text content
-
-      var originalText = "MIRËSEVINI";
-      // Function to add a letter to the text
-      function addLetter() {
-        var currentText = selfAddingTextElement.textContent;
-
-        var nextLetter = originalText[currentText.length];
-        currentText += nextLetter;
-
-        selfAddingTextElement.textContent = currentText;
-
-        // Stop the animation when all letters are added
-        if (currentText.length === originalText.length) {
-          clearInterval(intervalId);
-        }
-      }
-
-      var intervalId = setInterval(addLetter, 200);
-    </script>
-
     <script src="app.js"></script>
   </body>
 </html>
