233280031be6707df17e44c4005de17fc039f1a8
Dren Morina <drenmorina321@gmail.com>
2024-04-21 05:21:54 +0200

    Add code that writes data to file

--- counter.php
+++ counter.php
@@ -18,4 +18,23 @@ if (!isset($_SESSION['visit_counts'])) {
   echo "<br><br><br><br> TESTT <br><br><br>";
 
   print_r($_SESSION["visit_counts"]);
+
+
+  if (isset($_SESSION['visit_counts'])) {
+    $visit_data_str = serialize($_SESSION['visit_counts']);
+
+    $file_path = 'visit_data.txt';
+
+    $file_handle = fopen($file_path, 'w');
+
+    if ($file_handle) {
+        fwrite($file_handle, $visit_data_str);
+
+        fclose($file_handle);
+    } else {
+        echo "Error: Unable to open file for writing.";
+    }
+} else {
+    echo "Error: visit_counts session array doesn't exist.";
+}
 ?>
