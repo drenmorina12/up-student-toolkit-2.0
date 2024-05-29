098a1d27c0a7d72a3ece37d7e0bb49c97b39e080
BlertaLutolli <118124912+BlertaLutolli@users.noreply.github.com>
2024-05-29 20:41:46 +0200

    Fix ContactUs styling

--- rreth-nesh/rreth-nesh.php
+++ rreth-nesh/rreth-nesh.php
@@ -22,7 +22,7 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css">
 </head>
 
-  <body>
+  <body style="background-color: var(--primary-color);">
     <?php
       include("../header.php");
       include("../counter.php");
