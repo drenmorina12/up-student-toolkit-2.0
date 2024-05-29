92f00ad8f89fb2bd8ef51ebe39e4da69f4f6f3dd
Endrit Kastrati <endritkastrati8@gmail.com>
2024-05-29 21:50:23 +0200

    Adding weather API at info page

--- migrations/queries.sql
+++ migrations/queries.sql
@@ -33,34 +33,37 @@ CREATE TABLE IF NOT EXISTS `up_studenttoolkit`.`tblasociacioni` (
 
 
 
-START TRANSACTION;
 
-CREATE TABLE `opsionet` (
-  `id` int(11) NOT NULL,
-  `opsioni` text NOT NULL,
-  `pyetja_id` int(11) NOT NULL,
-  `sakte` tinyint(1) NOT NULL
-); 
+-- MOS E EKZEKUTONI:
 
+-- START TRANSACTION;
 
-CREATE TABLE `pyetjet` (
-  `id` int(11) NOT NULL,
-  `pyetja` text NOT NULL
-);
+-- CREATE TABLE `opsionet` (
+--   `id` int(11) NOT NULL,
+--   `opsioni` text NOT NULL,
+--   `pyetja_id` int(11) NOT NULL,
+--   `sakte` tinyint(1) NOT NULL
+-- ); 
 
 
--- SHTIMI I PRIMARY-KEY:
-ALTER TABLE `opsionet`
-  ADD PRIMARY KEY (`id`);
-
-ALTER TABLE `pyetjet`
-  ADD PRIMARY KEY (`id`);
+-- CREATE TABLE `pyetjet` (
+--   `id` int(11) NOT NULL,
+--   `pyetja` text NOT NULL
+-- );
 
 
--- SHTIMI I AUTO-INCREMENT:
-ALTER TABLE `opsionet`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=146;
+-- -- SHTIMI I PRIMARY-KEY:
+-- ALTER TABLE `opsionet`
+--   ADD PRIMARY KEY (`id`);
 
-ALTER TABLE `pyetjet`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
-COMMIT; 
+-- ALTER TABLE `pyetjet`
+--   ADD PRIMARY KEY (`id`);
+
+
+-- -- SHTIMI I AUTO-INCREMENT:
+-- ALTER TABLE `opsionet`
+--   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=146;
+
+-- ALTER TABLE `pyetjet`
+--   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+-- COMMIT; 
