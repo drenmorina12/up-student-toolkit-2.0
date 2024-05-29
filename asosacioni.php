ee3dbb73b7229dfd4fd11be0cd5ec134fa79cde2
Dren Morina <drenmorina321@gmail.com>
2024-05-29 22:01:25 +0200

    Use references at asosacioni.php and set limit per accont to 7

--- asosacioni.php
+++ asosacioni.php
@@ -1,91 +1,78 @@
 <?php
-  
-  session_start();
 
-  include('db.php');
+session_start();
 
-  $data = file_get_contents('php://input');
-  $decoded_data = json_decode($data, true);
+include('db.php');
 
-  $asosacioni = $decoded_data['asosacioni'];
+const MAX_ASOSACIONES = 7;
 
-  $userId = $_SESSION["user-id"];
+$data = file_get_contents('php://input');
+$decoded_data = json_decode($data, true);
 
-  
-  // Access individual elements
-  $a1 = $asosacioni['A'][0];
-  $a2 = $asosacioni['A'][1];
-  $a3 = $asosacioni['A'][2];
-  $a4 = $asosacioni['A'][3];
-  $zgjidhjaA = $asosacioni['A'][4];
-  $zgjidhjaPerfundimtare = $asosacioni['A'][5];
-  $titulli = $asosacioni['A'][6];
+$asosacioni = $decoded_data['asosacioni'];
 
-  $b1 = $asosacioni['B'][0];
-  $b2 = $asosacioni['B'][1];
-  $b3 = $asosacioni['B'][2];
-  $b4 = $asosacioni['B'][3];
-  $zgjidhjaB = $asosacioni['B'][4];
+$userId = $_SESSION["user-id"];
 
-  $c1 = $asosacioni['C'][0];
-  $c2 = $asosacioni['C'][1];
-  $c3 = $asosacioni['C'][2];
-  $c4 = $asosacioni['C'][3];
-  $zgjidhjaC = $asosacioni['C'][4];
+$query = "SELECT COUNT(*) as total FROM tblasociacioni WHERE userId = ?";
+$stmt = $conn->prepare($query);
+$stmt->bind_param("i", $userId);
+$stmt->execute();
+$result = $stmt->get_result();
+$row = $result->fetch_assoc();
+$currentCount = $row['total'];
+$stmt->close();
 
-  $d1 = $asosacioni['D'][0];
-  $d2 = $asosacioni['D'][1];
-  $d3 = $asosacioni['D'][2];
-  $d4 = $asosacioni['D'][3];
-  $zgjidhjaD = $asosacioni['D'][4];
+if ($currentCount < MAX_ASOSACIONES){
+  // Using references to access individual elements
+  $a1 = &$asosacioni['A'][0];
+  $a2 = &$asosacioni['A'][1];
+  $a3 = &$asosacioni['A'][2];
+  $a4 = &$asosacioni['A'][3];
+  $zgjidhjaA = &$asosacioni['A'][4];
+  $zgjidhjaPerfundimtare = &$asosacioni['A'][5];
+  $titulli = &$asosacioni['A'][6];
 
-  $query = 
+  $b1 = &$asosacioni['B'][0];
+  $b2 = &$asosacioni['B'][1];
+  $b3 = &$asosacioni['B'][2];
+  $b4 = &$asosacioni['B'][3];
+  $zgjidhjaB = &$asosacioni['B'][4];
+
+  $c1 = &$asosacioni['C'][0];
+  $c2 = &$asosacioni['C'][1];
+  $c3 = &$asosacioni['C'][2];
+  $c4 = &$asosacioni['C'][3];
+  $zgjidhjaC = &$asosacioni['C'][4];
+
+  $d1 = &$asosacioni['D'][0];
+  $d2 = &$asosacioni['D'][1];
+  $d3 = &$asosacioni['D'][2];
+  $d4 = &$asosacioni['D'][3];
+  $zgjidhjaD = &$asosacioni['D'][4];
 
   $stmt = $conn->prepare("INSERT INTO tblasociacioni (userId, titulli, A1, A2, A3, A4, Afinal, B1, B2, B3, B4, Bfinal, C1, C2, C3, C4, Cfinal, D1, D2, D3, D4, Dfinal, final) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
   $stmt->bind_param("issssssssssssssssssssss",
-    $userId,
-    $titulli,
-    $a1, $a2, $a3, $a4, $zgjidhjaA,
-    $b1, $b2, $b3, $b4, $zgjidhjaB,
-    $c1, $c2, $c3, $c4, $zgjidhjaC,
-    $d1, $d2, $d3, $d4, $zgjidhjaD,
-    $zgjidhjaPerfundimtare
-);
+      $userId,
+      $titulli,
+      $a1, $a2, $a3, $a4, $zgjidhjaA,
+      $b1, $b2, $b3, $b4, $zgjidhjaB,
+      $c1, $c2, $c3, $c4, $zgjidhjaC,
+      $d1, $d2, $d3, $d4, $zgjidhjaD,
+      $zgjidhjaPerfundimtare
+  );
 
-if ($stmt->execute()) {
-  echo "New records created succesfully";
+  if ($stmt->execute()) {
+      echo "New records created successfully";
+  } else {
+      echo "Error: " . $stmt->error;
+  }
+
+  $stmt->close();
 } else {
-  echo "Error: " . $stmt->error;
+    echo "You already have 7 asosaciones. You cannot add more.";
 }
 
-$stmt->close();
+
 $conn->close();
-
-
-  // Print individual elements for debugging purposes
-  // echo "A1: " . $a1 . "<br>";
-  // echo "A2: " . $a2 . "<br>";
-  // echo "A3: " . $a3 . "<br>";
-  // echo "A4: " . $a4 . "<br>";
-  // echo "Zgjidhja A: " . $zgjidhjaA . "<br>";
-  // echo "Zgjidhja Perfundimtare: " . $zgjidhjaPerfundimtare . "<br>";
-
-  // echo "B1: " . $b1 . "<br>";
-  // echo "B2: " . $b2 . "<br>";
-  // echo "B3: " . $b3 . "<br>";
-  // echo "B4: " . $b4 . "<br>";
-  // echo "Zgjidhja B: " . $zgjidhjaB . "<br>";
-
-  // echo "C1: " . $c1 . "<br>";
-  // echo "C2: " . $c2 . "<br>";
-  // echo "C3: " . $c3 . "<br>";
-  // echo "C4: " . $c4 . "<br>";
-  // echo "Zgjidhja C: " . $zgjidhjaC . "<br>";
-
-  // echo "D1: " . $d1 . "<br>";
-  // echo "D2: " . $d2 . "<br>";
-  // echo "D3: " . $d3 . "<br>";
-  // echo "D4: " . $d4 . "<br>";
-  // echo "Zgjidhja D: " . $zgjidhjaD . "<br>";
 
 ?>
